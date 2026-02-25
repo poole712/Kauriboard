2c63daeeaa3a5d87285524e5f0145dc89b2b8cfa
poole712 <fpoole666@gmail.com>
2026-02-25 15:41:13 +1300

    Home page mostly done

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,17 +1,47 @@
+import { Link } from "react-router";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div className="jumbotron align-self-center vh-100">
+      <div className="kauriJumbotron p-3">
         <h1>Welcome to KauriTree!</h1>
-        <div className="d-flex flex-row p-3 m-3">
-          <p className="col-md-6 m-2 fs-3 align-self-center text-start">
-            KauriTree is a project planning software focused on collaboration
-            and productivity.
-          </p>
-          <img className="kauriTree m-2" src="./HomePageKauri.jpg" alt="KauriTree" />
+        <div className="aboutKauriTree" >
+          <div className="aboutKauriText">
+            <h3 className="mb-3">What is KauriTree?</h3>
+            <p className="fs-5 align-self-center ">
+              KauriTree is a project planning software focused on collaboration
+              and productivity. Manage all your projects and projects tasks in a simple board layout.
+              <br></br>
+              <br></br>
+              Allocate tasks to different users as well as organising them in a simple 3 board (branch) system which
+              results in a easy to learn and master interface.
+            </p>
+            <Link className="btn btn-primary mt-4" to={'/pages/Projects'}>Get Planning!</Link>
+          </div>
+          <img className="kauriTree" src="./HomePageKauri.jpg" alt="KauriTree" />
         </div>
+
+        <div className="aboutKauriTree" >
+          <div className="aboutKauriText">
+            <h3 className="mb-3">More about/from me</h3>
+            <p className="fs-5 align-self-center ">
+              My name is Fletcher Poole and I am based in Auckland, New Zealand. I have a strong passion
+              for Game and Web Development. In 2025 I completed a degree in Computer Science, Majoring in Software Development
+              as Auckland University of Technology (AUT).
+              <br></br>
+              <br></br>
+              Below are links to my Portfolios, GitHub as well as my contact details. Thanks for checking out KauriTree!
+            </p>
+            <div className="pt-2">
+              <a className="btn btn-primary m-1" target="_blank" href="https://github.com/poole712?tab=repositories">GitHub<i className="bi bi-github px-1"></i></a>
+              <a className="btn btn-primary m-1" target="_blank" href="https://fletchersgamedesign.com/">Game Portfolio<i className="bi bi-joystick px-1"></i></a>
+              <a className="btn btn-primary m-1" target="_blank" >Web Portfolio<i className="bi bi-globe2 px-1"></i></a>
+            </div>
+          </div>
+          <img className="profilePhoto" src="./Me.png" alt="Me" />
+        </div>
+
       </div>
     </>
   );
