101f823264e6aa53718d79fe7740af295bd9a66e
poole712 <54291199+poole712@users.noreply.github.com>
2026-03-13 11:08:52 +1300

    Updated Base URL to use Azure

--- Frontend/src/API/axios.ts
+++ Frontend/src/API/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5017/api',
+    baseURL: 'https://kauriboard-c4fvc3dkhfg7d6ey.canadacentral-01.azurewebsites.net/',
     timeout: 5000,
     headers: {'Content-Type': 'application/json'}
 });
